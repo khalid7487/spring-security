f35cd1ef98da25ec929cb868f0ece7314953117a
khalid7487 <khalid15-7487@diu.edu.bd>
2021-05-24 14:33:40 +0600

    add fake databese authentication

--- jwt/src/main/java/com/jwt/jwt/auth/ApplicationUserService.java
+++ jwt/src/main/java/com/jwt/jwt/auth/ApplicationUserService.java
@@ -1,5 +1,7 @@
 package com.jwt.jwt.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,7 +11,8 @@ import org.springframework.stereotype.Service;
 public class ApplicationUserService implements UserDetailsService {
     private final ApplicationUserDao applicationUserDao;
 
-    public ApplicationUserService(ApplicationUserDao applicationUserDao) {
+    @Autowired
+    public ApplicationUserService(@Qualifier("fake") ApplicationUserDao applicationUserDao) {
         this.applicationUserDao = applicationUserDao;
     }
 
--- jwt/src/main/java/com/jwt/jwt/security/ApplicationSecurityConfig.java
+++ jwt/src/main/java/com/jwt/jwt/security/ApplicationSecurityConfig.java
@@ -1,24 +1,20 @@
 package com.jwt.jwt.security;
 
+import com.jwt.jwt.auth.ApplicationUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.jwt.jwt.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.jwt.jwt.security.ApplicationUserRole.*;
 
 @Configuration
@@ -27,10 +23,12 @@ import static com.jwt.jwt.security.ApplicationUserRole.*;
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
 
     @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService) {
         this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
     }
 
     @Override
@@ -67,33 +65,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails khalidUser = User.builder()
-                .username("khalid")
-                .password(passwordEncoder.encode("password"))
-    //            .roles(STUDENT.name())
-                .authorities(STUDENT.getGrantedAuthorities())
-                .build();
-
-        UserDetails amanulUser = User.builder()
-                .username("amanul")
-                .password(passwordEncoder.encode("password123"))
-    //            .roles(ADMIN.name())
-                .authorities(ADMIN.getGrantedAuthorities())
-                .build();
-        UserDetails rakibUser = User.builder()
-                .username("rakib")
-                .password(passwordEncoder.encode("password123"))
-    //            .roles(ADMINTRAINEE.name())
-                .authorities(ADMINTRAINEE.getGrantedAuthorities())
-                .build();
-
-
-        return new InMemoryUserDetailsManager(khalidUser,
-                amanulUser, rakibUser);
-
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
+    }
+
 
 
 }
